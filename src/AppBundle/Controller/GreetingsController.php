8c62cf9aa59f905b11e65ae055a20dfc75914dcf
Benoit <benoit.colin@web-atrio.com>
2017-03-15 12:21:41 +0100

    "fichiers WABlog"

---
+++ src/AppBundle/Controller/GreetingsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as FOSRestBundleAnnotations;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use AppBundle\Hello;
+
+/**
+ * @FOSRestBundleAnnotations\View()
+ */
+class GreetingsController extends FOSRestController implements ClassResourceInterface
+{
+    public function helloAction()
+    {
+        return new Hello();
+    }
+}
+
